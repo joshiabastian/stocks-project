76e7695fb53e7b7c453c22c4b86c682ffb9c5457
byMacho <j.bastian21197@gmail.com>
2026-02-12 17:08:25 +0700

    Update: Stock-Project

--- scripts/extract.py
+++ scripts/extract.py
@@ -7,3 +7,12 @@ from datetime import datetime
 def fetch_stock_data(tickers, start_date, end_date):
     """Mengambil data saham"""
     log.info(f"Mulai mengambil data saham {tickers} dari {start_date} sampai {end_date}")
+    
+    stock_data = yf.download(tickers, start=start_date, end=end_date, group_by='ticker', auto_adjust=True)
+    
+    if stock_data.empty:
+        log.warning("Tidak ada data yang diambil. Periksa simbol saham atau rentang tanggal.")
+        return None
+    
+    log.info("Pengambilan data selesai.")
+    return stock_data
