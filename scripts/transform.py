c3ffe8ba79b7e548d23763b837868829935ee61c
byMacho <j.bastian21197@gmail.com>
2026-02-18 11:43:59 +0700

    Update: Stocks-Project

--- scripts/transform.py
+++ scripts/transform.py
@@ -8,7 +8,7 @@ log = logging.getLogger(__name__)
 REQUIRED_COLUMNS = ["trade_date", "ticker", "open_price", "high_price",
                     "low_price", "close_price", "volume", "daily_change_pct"]
 
-
+# func untuk transform data yang sudah di extract
 def transform_stock_data(stock_data: pd.DataFrame, tickers: list[str]) -> pd.DataFrame:
     """
     Transformasi DataFrame hasil yfinance menjadi format siap simpan ke DB.
